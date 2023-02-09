cb000f94fb7e43c1865ab838a09ffdd3b2c09a86
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-02-09 08:26:41 -0500

    Stuff

--- qa.py
+++ qa.py
@@ -5,18 +5,20 @@ from langchain.chains import VectorDBQAWithSourcesChain
 import pickle
 import argparse
 
-parser = argparse.ArgumentParser(description='Ask a question to the notion DB.')
-parser.add_argument('question', type=str, help='The question to ask the notion DB')
+parser = argparse.ArgumentParser(description="Ask a question to the docs DB.")
+parser.add_argument("question", type=str, help="The question to ask the docs DB")
 args = parser.parse_args()
 
 # Load the LangChain.
 index = faiss.read_index("docs.index")
-
 with open("faiss_store.pkl", "rb") as f:
     store = pickle.load(f)
-
 store.index = index
-chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
+
+chain = VectorDBQAWithSourcesChain.from_llm(
+    llm=OpenAI(temperature=1.0), vectorstore=store
+)
+
 result = chain({"question": args.question})
 print(f"Answer: {result['answer']}")
 print(f"Sources: {result['sources']}")
